85024c34c2dcaf7c1d1f500d3268d08333a7c314
Thomas Leuxner <tlx@leuxner.net>
2017-10-04 15:34:18 +0200

    Adjusted code comments.

--- bin/upssl.sh
+++ bin/upssl.sh
@@ -4,13 +4,15 @@ set -e
 # to the server's SSL directories e.g. /etc/ssl/*
 # Thomas Leuxner <tlx@leuxner.net> 03-08-2017
 #
-# + added DANE RR Update Logic: DNS fingerprint 04-10-2017
+# [04-10-2017]
+# + added DANE RR Update Logic: DNS fingerprint
+#
 acme_certs_dir='/etc/dehydrated/certs'
 cert_file_ext='_ACME.pem'
 cert_keyfile_ext='_ACME.key'
 ssl_certs_dir='/etc/ssl/certs'
 ssl_certs_keydir='/etc/ssl/private'
-dane_keyfile="$ssl_certs_keydir/host_domain_tld_ACME.key"
+dane_keyfile="$ssl_certs_keydir/host_example_com_ACME.key"
 dane_ttl='900'
 dane_file_date_old=$(date -r $dane_keyfile +%s)
 dane_nsupdate_key='/root/Kxxxx.123+45678.private'
@@ -41,7 +43,6 @@ for i in $(find $acme_certs_dir -name privkey.pem)
 	{ msg_formatted "Updating $ssl_certs_keydir/$cert_file$cert_keyfile_ext..."; cp -p $i $ssl_certs_keydir/$cert_file$cert_keyfile_ext; \
 	chmod 600 $ssl_certs_keydir/$cert_file$cert_keyfile_ext >&2; }
 done
-
 
 	# Check whether mail server certficate has been renewed
 	dane_file_date_new=$(date -r $dane_keyfile +%s)
