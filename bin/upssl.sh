035c1cc5bc0b02347812d470b4d2c2a4e4ada275
Thomas Leuxner <tlx@leuxner.net>
2017-10-04 13:56:39 +0200

    Add DANE RR Fingerprint code.

--- bin/upssl.sh
+++ bin/upssl.sh
@@ -3,12 +3,17 @@ set -e
 # Copy certificates acquired by https://github.com/lukas2511/dehydrated 
 # to the server's SSL directories e.g. /etc/ssl/*
 # Thomas Leuxner <tlx@leuxner.net> 03-08-2017
-
+#
+# + added DANE RR Update Logic: DNS fingerprint 04-10-2017
 acme_certs_dir='/etc/dehydrated/certs'
 cert_file_ext='_ACME.pem'
 cert_keyfile_ext='_ACME.key'
 ssl_certs_dir='/etc/ssl/certs'
 ssl_certs_keydir='/etc/ssl/private'
+dane_keyfile="$ssl_certs_keydir/host_domain_tld_ACME.key"
+dane_ttl='900'
+dane_file_date_old=$(date -r $dane_keyfile +%s)
+dane_nsupdate_key='/root/Kxxxx.123+45678.private'
 
 msg_formatted() {
 	echo "[>] $*"
@@ -36,3 +41,12 @@ for i in $(find $acme_certs_dir -name privkey.pem)
 	{ msg_formatted "Updating $ssl_certs_keydir/$cert_file$cert_keyfile_ext..."; cp -p $i $ssl_certs_keydir/$cert_file$cert_keyfile_ext; \
 	chmod 600 $ssl_certs_keydir/$cert_file$cert_keyfile_ext >&2; }
 done
+
+
+	# Check whether mail server certficate has been renewed
+	dane_file_date_new=$(date -r $dane_keyfile +%s)
+	if [ $dane_file_date_new -gt $dane_file_date_old ]; then
+		dane_record=$(postfix tls output-server-tlsa $dane_keyfile | sed -e "1d;s/IN/$dane_ttl/")
+		msg_formatted "Updating DANE RR: $dane_record"
+		{ echo "update add $dane_record"; echo send; echo quit; } | nsupdate -v -k $dane_nsupdate_key
+	fi
